2f50f28589986e9c1a5666e9d1c90e6907205aaa
Hadeaji <hadeaji@gmail.com>
2021-10-22 11:49:14 +0300

    convert to responsive

---
+++ script/animations.js
@@ -0,0 +1,76 @@
+'use strict';
+
+
+const sections = ['p2', 'p3'];
+const icons = ['uno', 'dos', 'tres'];
+
+$("#uno").click(function () {
+    sections.forEach(element => {
+        if ($(`#${element}`).hasClass("showSection")) {
+            $(`#${element}`).toggleClass("showSection");
+        }
+    });
+
+    $("#p1").removeClass("blurOnShow");
+
+    icons.forEach(element => {
+        if (($(`#${element}`).hasClass("capIcon"))) {
+            $(`#${element}`).toggleClass("capIcon");
+        }
+        if (($(`#${element}`).hasClass("minIcon"))) {
+            $(`#${element}`).toggleClass("minIcon");
+        }
+    });
+});
+
+$("#dos").click(function () {
+    sections.forEach(element => {
+        if ($(`#${element}`).hasClass("showSection")) {
+            $(`#${element}`).toggleClass("showSection");
+        }
+    });
+
+    $("#p2").toggleClass("showSection");
+
+    $("#p1").addClass("blurOnShow");
+
+    icons.forEach(element => {
+        if (($(`#${element}`).hasClass("capIcon"))) {
+            $(`#${element}`).toggleClass("capIcon");
+            $(`#${element}`).toggleClass("minIcon");
+        }
+        if (!($(`#${element}`).hasClass("minIcon"))) {
+            $(`#${element}`).toggleClass("minIcon");
+        }
+    });
+    $("#dos").addClass("capIcon");
+});
+
+$("#tres").click(function () {
+    sections.forEach(element => {
+        if ($(`#${element}`).hasClass("showSection")) {
+            $(`#${element}`).toggleClass("showSection");
+        }
+    });
+
+    $("#p3").toggleClass("showSection");
+
+    $("#p1").addClass("blurOnShow");
+
+    icons.forEach(element => {
+        if (($(`#${element}`).hasClass("capIcon"))) {
+            $(`#${element}`).toggleClass("capIcon");
+            $(`#${element}`).toggleClass("minIcon");
+        }
+        if (!($(`#${element}`).hasClass("minIcon"))) {
+            $(`#${element}`).toggleClass("minIcon");
+        }
+    });
+    $("#tres").addClass("capIcon");
+});
+
+// $( "#mydiv" ).hasClass( "showSection" )
+
+// $(button).addClass("blurOnShow")
+
+// $("p").toggleClass("main");
